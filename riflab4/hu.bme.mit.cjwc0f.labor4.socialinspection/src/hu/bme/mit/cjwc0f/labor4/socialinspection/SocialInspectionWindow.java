d8fa9daafe5c258e6113aebbe6d098d91b4f130f
Marton Bur <marton.bur@gmail.com>
2015-03-29 03:13:42 +0200

    Created a GUI version for social inspection task that doesn't block the UI

--- riflab4/hu.bme.mit.cjwc0f.labor4.socialinspection/src/hu/bme/mit/cjwc0f/labor4/socialinspection/SocialInspectionWindow.java
+++ riflab4/hu.bme.mit.cjwc0f.labor4.socialinspection/src/hu/bme/mit/cjwc0f/labor4/socialinspection/SocialInspectionWindow.java
@@ -8,8 +8,6 @@ import hu.bme.mit.cjwc0f.labor4.workflow.SocialInspection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.management.ManagementFactory;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
@@ -18,14 +16,45 @@ import javax.jms.QueueReceiver;
 import javax.jms.QueueSender;
 import javax.jms.QueueSession;
 import javax.naming.NamingException;
+import javax.swing.SwingWorker;
 
 @SuppressWarnings("serial")
 public class SocialInspectionWindow extends AbstractWindow {
 	
+	private final class SwingWorkerExtension extends SwingWorker<Void, Void> {
+		@Override
+		protected Void doInBackground() throws Exception {
+			button.setEnabled(false);
+			if(receivedMessage == null){
+				receivedMessage = (ObjectMessage) receiver.receiveNoWait();
+			}
+			while (receivedMessage == null) {
+				Thread.sleep(200);
+				receivedMessage = (ObjectMessage) receiver.receiveNoWait();
+			}
+			ApplicationData applicantData = (ApplicationData) receivedMessage.getObject();
+			SocialResult socialResult = SocialInspection.createResult(applicantData);
+			ObjectMessage message = session.createObjectMessage(socialResult);
+			sender.send(message);
+			textArea.setText(socialResult.toString());
+			
+			receivedMessage = (ObjectMessage) receiver.receiveNoWait();
+			while (receivedMessage == null) {
+				Thread.sleep(200);
+				receivedMessage = (ObjectMessage) receiver.receiveNoWait();
+			}
+			button.setEnabled(true);
+			return null;
+		}
+
+	}
+
 	private Queue inputQueue;
 	private QueueSender sender;
 	private Queue outputQueue;
 	private QueueReceiver receiver;
+	private ObjectMessage receivedMessage;
+	private boolean firstClick = true;
 	
 	public SocialInspectionWindow() {
 		super("Social inspection", 700, 500);
@@ -47,21 +76,22 @@ public class SocialInspectionWindow extends AbstractWindow {
 			e1.printStackTrace();
 		}
 
+		receivedMessage = null;
 		
 		button.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				try {
-					ObjectMessage receive = ((ObjectMessage)receiver.receive());
-					ApplicationData applicantData = (ApplicationData) receive.getObject();
-					SocialResult socialResult = SocialInspection.createResult(applicantData);
-		        	ObjectMessage message = session.createObjectMessage(socialResult);
-					sender.send(message);
-					textArea.setText(socialResult.toString());
-				} catch (JMSException e1) {
-					Logger.global.log(Level.SEVERE, "Could not publish message: " + e1.getMessage());
+				SwingWorker<Void, Void> messageReader = new SwingWorkerExtension();
+				if (firstClick) {
+					// Only for init
+					firstClick = false;
+					messageReader.execute();
+					if(receivedMessage == null){
+						textArea.setText("The first input is not ready yet.");
+					}
+				} else {
+					messageReader.execute();
 				}
 			}
 			

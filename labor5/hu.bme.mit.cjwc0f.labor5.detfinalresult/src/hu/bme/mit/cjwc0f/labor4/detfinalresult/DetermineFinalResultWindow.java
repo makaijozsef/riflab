bda4e854f3ea1785ff3e4857a80b8ea3b4945c22
Jozsef Makai <reyuulol@gmail.com>
2015-04-10 00:36:06 +0200

    final result

--- labor5/hu.bme.mit.cjwc0f.labor5.detfinalresult/src/hu/bme/mit/cjwc0f/labor4/detfinalresult/DetermineFinalResultWindow.java
+++ labor5/hu.bme.mit.cjwc0f.labor5.detfinalresult/src/hu/bme/mit/cjwc0f/labor4/detfinalresult/DetermineFinalResultWindow.java
@@ -8,25 +8,23 @@ import hu.bme.mit.cjwc0f.labor5.workflow.DetermineFinalResult;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.Serializable;
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Queue;
 
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class DetermineFinalResultWindow extends AbstractWindow {
 	
-	
-	
 	private List<ApplicationData> applicationDatas = new ArrayList<ApplicationData>();
 	private List<SocialResult> socialResults = new ArrayList<SocialResult>();
 
 	private JButton socialButton;
 	
-	public DetermineFinalResultWindow() {
+	public DetermineFinalResultWindow(Queue<Serializable> inQueueStudy, Queue<Serializable> inQueueSocial) {
 		
 		super("Determine final result", 1400, 300);
 		
@@ -38,7 +36,24 @@ public class DetermineFinalResultWindow extends AbstractWindow {
 		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				ApplicationData applicationData = (ApplicationData)inQueueStudy.poll();
 				
+				SocialResult matchingSR = null;
+				for (SocialResult sr : socialResults) {
+					if(applicationData.getTimestamp().equals(sr.getApplicantData().getTimestamp())){
+						matchingSR = sr;
+						break;
+					}
+				}
+				if(matchingSR != null){
+					socialResults.remove(matchingSR);
+					ApplicationData finalResult = DetermineFinalResult.decide(applicationData, matchingSR);
+					
+					textArea.setText(finalResult.toString());
+				}
+				else {
+					applicationDatas.add(applicationData);
+				}
 			}
 
 		});
@@ -52,6 +67,24 @@ public class DetermineFinalResultWindow extends AbstractWindow {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				SocialResult socialResult = (SocialResult)inQueueSocial.poll();
+				
+				ApplicationData matchingAD = null;
+				for (ApplicationData ad : applicationDatas) {
+					if(ad.getTimestamp().equals(socialResult.getApplicantData().getTimestamp())){
+						matchingAD = ad;
+						break;
+					}
+				}
+				if(matchingAD != null){
+					applicationDatas.remove(matchingAD);
+					ApplicationData finalResult = DetermineFinalResult.decide(matchingAD, socialResult);
+					
+					textArea.setText(finalResult.toString());
+				}
+				else {
+					socialResults.add(socialResult);
+				}
 			}
 
 		});

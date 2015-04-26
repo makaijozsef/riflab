86644ea1099594aeeeb2f8b0afb2bc32b7b7e479
Jozsef Makai <reyuulol@gmail.com>
2015-04-26 11:29:23 +0200

    Rules are fired, integration phase complete, can we write the rules?

--- labor6/hu.bme.mit.cjwc0f.labor6.main/src/main/java/hu/bme/mit/cjwc0f/labor6/main/DroolsListener.java
+++ labor6/hu.bme.mit.cjwc0f.labor6.main/src/main/java/hu/bme/mit/cjwc0f/labor6/main/DroolsListener.java
@@ -1,20 +1,31 @@
 package hu.bme.mit.cjwc0f.labor6.main;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
 import hu.bme.mit.cjwc0f.events.HostelApplicationAbstractEvent;
 import hu.bme.mit.cjwc0f.labor5.drools.EventQueue;
 import hu.bme.mit.cjwc0f.labor5.drools.IDroolsListener;
 
 public class DroolsListener implements IDroolsListener {
+	
+	private KieServices kie;
+    private KieSession kSession;
 
 	public DroolsListener() {
-		// TODO Auto-generated constructor stub
+		kie = KieServices.Factory.get();
+	    KieContainer kContainer = kie.getKieClasspathContainer();
+    	kSession = kContainer.newKieSession("ksession-rules");
 	}
 	
 	@Override
 	public void process() {
 		// TODO add this to the working memory
 		HostelApplicationAbstractEvent event = EventQueue.poll();
-		
+		System.out.println(event.toString());
+		kSession.insert(event);
+		kSession.fireAllRules();
 	}
 
 }

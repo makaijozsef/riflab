bd112161844b5de4fb84ab122f1a854e4b4f4489
Jozsef Makai <reyuulol@gmail.com>
2015-04-26 15:36:05 +0200

    New event classes

---
+++ labor5/hu.bme.mit.cjwc0f.labor5.common/src/hu/bme/mit/cjwc0f/events/DetFinalResultChosen.java
@@ -0,0 +1,5 @@
+package hu.bme.mit.cjwc0f.events;
+
+public class DetFinalResultChosen {
+
+}
---
+++ labor5/hu.bme.mit.cjwc0f.labor5.common/src/hu/bme/mit/cjwc0f/events/SocialWaitsForJoin.java
@@ -0,0 +1,7 @@
+package hu.bme.mit.cjwc0f.events;
+
+public class SocialWaitsForJoin extends HostelApplicationAbstractEvent{
+	public SocialWaitsForJoin(String timestamp) {
+		super(timestamp);
+	}
+}
